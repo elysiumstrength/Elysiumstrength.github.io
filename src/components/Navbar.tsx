4d248dbdd1a4778a47c37759a5a46cd4625d0d50
Wesley Kam <wesley@humun.org>
2025-06-20 20:41:00 -0700

    nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,22 +2,25 @@
 import { Link, NavLink } from 'react-router-dom'
 import './Navbar.css' // optional if you're styling with CSS
 
+import icon from '../assets/elysium_icon.png'
+
 export default function Navbar() {
-  return (
-    <header className="navbar">
-      <div className="navbar__container">
-        <Link to="/" className="navbar__logo">
-          MyApp
-        </Link>
-        <nav className="navbar__nav">
-          <NavLink to="/" end className="navbar__link">
-            Home
-          </NavLink>
-          <NavLink to="/about" className="navbar__link">
-            About
-          </NavLink>
-        </nav>
-      </div>
-    </header>
-  )
+    return (
+        <header className="navbar">
+            <div className="navbar__container">
+                <Link to="/" className="navbar__logo">
+                    <img src={icon} alt="Elysium Icon" className="navbar__icon" />
+                    Elysium
+                </Link>
+                <nav className="navbar__nav">
+                    <NavLink to="/" end className="button__tertiary">
+                        Home
+                    </NavLink>
+                    <NavLink to="/Consult" className="button__tertiary">
+                        Consult
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
+    )
 }
