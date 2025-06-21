e5137306f17b7c7743c62dae008d4ee571509163
Wesley Kam <wesley@humun.org>
2025-06-20 18:49:15 -0700

    fix template

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'  // ✅ use this!
 
-import './App.css'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
