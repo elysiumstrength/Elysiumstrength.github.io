f7f05b1cb540c0d12dc25fe8b811e68e4f6c2475
Wesley Kam <wesley@humun.org>
2025-07-12 16:22:47 -0700

    not found nav

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,18 @@
-// src/pages/Home.tsx
-export default function Home() {
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+import './NotFound.css'
+
+
+// src/pages/NotFound.tsx
+export default function NotFound() {
   return (
-    <section>
-      <h1>Not Found</h1>
-    </section>
+    <>
+      <Navbar />
+      <section className="not-found">
+        <h1>Not Found</h1>
+      </section>
+      <Footer />
+    </>
   )
 }
