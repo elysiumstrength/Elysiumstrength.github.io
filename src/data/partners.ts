1255216b68cbb27b4aad693d0daa7b551889de65
Wesley Kam <wesley@humun.org>
2025-09-10 07:49:29 -0700

    add bissell clinic and vital motion chiropractic

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -14,7 +14,7 @@ export const PARTNERS: Partner[] = [
         category: "Medical & Wellness",
         description:
             "At Gagneja Health, we believe in whole-body health. We hope to work with you closely on various aspects of your health, and we emphasize the impact that lifestyle, diet, and environmental factors have on your health.",
-        website: "gagnejahealth.com",
+        website: "https://gagnejahealth.com",
         logoImage: "/src/assets/GAGNEJA HEALTH.svg",
     },
     {
@@ -23,7 +23,25 @@ export const PARTNERS: Partner[] = [
         category: "Mental Health",
         description:
             "We are a group of psychotherapists and physicians who provide quality mental health care. As a mental health center, we assist client's by providing therapy, coordinated medication management with psychiatrists, coaching, and dreamwork.",
-        website: "insighttherapy.com",
+        website: "https://www.insighttherapy.center/",
         logoImage: "/src/assets/Insight Therapy.webp",
     },
+    {
+        id: 3,
+        name: "Vital Motion Chiropractic",
+        category: "Chiropractic",
+        description:
+            "Dr. Alex is a National Board Certified Doctor of Chiropractic who wants to help you take control of your body wellness. She loves helping active individuals optimize performance and be pain free through diversified chiropractic adjustments, professional level body work and personalized rehab programs.",
+        website: "https://linktr.ee/camarillochiropractic",
+        logoImage: "/src/assets/Vital_Motion_Chiropractic.webp",
+    },
+    {
+        id: 4,
+        name: "Bissell Clinic",
+        category: "Physical Therapy",
+        description:
+            "At Bissell Clinic, we also offer individualized plans of care for physical therapy. Whether you are recovering from an injury, rehabbing after surgery, or just want to develop more strength and flexibility, our expert Physical Therapists can help.",
+        website: "https://www.bissellclinic.com/",
+        logoImage: "/src/assets/Bissell_Clinic.png",
+    }
 ];
