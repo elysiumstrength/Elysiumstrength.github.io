d71d3699a3ba2b2e5971c2497a173df4b0e77e99
elysiumstrength <anna@elysium.training>
2025-10-16 10:46:44 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,6 +19,7 @@ export default function Footer() {
             <div className="contact__info">
                 <div className="contact">
                     <div className="address">
+                        <div classname="phone">
                         {/* 187 S Patterson Ave, Goleta, CA 93111 */}
                         <p>187 S Patterson Ave</p>
                         <p>Goleta, CA 93111</p>
